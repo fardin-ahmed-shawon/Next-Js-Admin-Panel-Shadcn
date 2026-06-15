2f51a4715cbfab713eb55d1a1c10f760e685ca70
fardin-ahmed-shawon <fardinahmed7443@gmail.com>
2026-06-15 11:32:08 +0600

    updated

--- src/app/(main)/auth/v2/login/page.tsx
+++ src/app/(main)/auth/v2/login/page.tsx
@@ -1,11 +1,8 @@
-import Link from "next/link";
-
 import { Globe } from "lucide-react";
 
 import { APP_CONFIG } from "@/config/app-config";
 
 import { LoginForm } from "../../_components/login-form";
-import { GoogleButton } from "../../_components/social-auth/google-button";
 
 export default function LoginV2() {
   return (
@@ -16,22 +13,11 @@ export default function LoginV2() {
           <p className="text-muted-foreground text-sm">Please enter your details to login.</p>
         </div>
         <div className="space-y-4">
-          <GoogleButton className="w-full" />
-          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-border after:border-t">
-            <span className="relative z-10 bg-background px-2 text-muted-foreground">Or continue with</span>
-          </div>
           <LoginForm />
         </div>
       </div>
 
-      <div className="absolute top-5 flex w-full justify-end px-10">
-        <div className="text-muted-foreground text-sm">
-          Don&apos;t have an account?{" "}
-          <Link prefetch={false} className="text-foreground" href="register">
-            Register
-          </Link>
-        </div>
-      </div>
+
 
       <div className="absolute bottom-5 flex w-full justify-between px-10">
         <div className="text-sm">{APP_CONFIG.copyright}</div>
